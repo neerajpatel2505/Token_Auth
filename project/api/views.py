4d6615008c2e22ad2433f8bd570d2a4719a3ede9
Neeraj <nkurmbanshi@yahoo.com>
2024-04-30 15:26:53 +0530

    Token Authentication

---
+++ project/api/views.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.models import User
+from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import *
+from .models import *
+from rest_framework.permissions import IsAuthenticated,IsAdminUser
+# from rest_framework.permissions import AllowAny
+# from rest_framework.permissions import IsAdminUser
+# from rest_framework.permissions import IsAuthenticatedOrReadOnly
+# from rest_framework.authtoken.models import Token
+# user = User.objects.all()
+# for i in user:
+# 	token = Token.objects.get_or_create(user=i)
+# 	print(token)
+
+
+# create a viewset
+# class MovieViewSet(viewsets.ReadOnlyModelViewSet):
+class MovieViewSet(viewsets.ModelViewSet):
+	permission_classes = [IsAuthenticated]
+	# permission_classes = [AllowAny]
+	# permission_classes = [IsAdminUser]
+	# permission_classes = [IsAuthenticatedOrReadOnly]
+	
+	# define queryset
+	queryset = MovieModel.objects.all()
+	serializer_class = MovieSerializer
+
+class StudentViewSet(viewsets.ModelViewSet):
+	permission_classes = [IsAdminUser]
+	queryset = StudentModel.objects.all()
+	serializer_class = StudentSerializer
